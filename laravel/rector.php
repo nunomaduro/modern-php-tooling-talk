b0b2cf44d25501aabbc7626b4e4edf4563e90125
Nuno Maduro <enunomaduro@gmail.com>
2025-03-02 13:34:30 +0000

    wip

--- laravel/rector.php
+++ laravel/rector.php
@@ -3,8 +3,12 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 
 return RectorConfig::configure()
+    ->withSkip([
+        AddOverrideAttributeToOverriddenMethodsRector::class,
+    ])
     ->withPaths([
         __DIR__.'/app',
     ])
